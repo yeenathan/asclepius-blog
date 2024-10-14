a88af5500cc521374714cdb516155a21f7683e34
Nathan <yeenathan21@gmail.com>
2024-10-14 14:14:09 -0700

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import cn from "classnames";
